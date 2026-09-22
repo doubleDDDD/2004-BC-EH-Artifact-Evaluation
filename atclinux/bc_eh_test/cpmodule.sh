9e6557623fa47a0849155d74af795739d119c6de
doubled <18374822143@163.com>
2026-09-22 10:19:44 +0800

    init

--- atclinux/bc_eh_test/cpmodule.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -eu
-
-cp -f ../drivers/scsi/scsi_debug.ko build_ramdisk/initrmfs/double_D/modules/
-cp -f ../lib/crc/crc-t10dif.ko build_ramdisk/initrmfs/double_D/modules/
-# cp ../drivers/scsi/iscsi_tcp.ko build_ramdisk/initrmfs/double_D/modules/
-cp -f ../drivers/scsi/mpt3sas/mpt3sas.ko build_ramdisk/initrmfs/double_D/modules/
-cp -f ../drivers/scsi/megaraid/megaraid_sas.ko build_ramdisk/initrmfs/double_D/modules/
